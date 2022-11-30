a857b115537717910f4d554eff4875f10e6de368
felix <59274976+felixvrs@users.noreply.github.com>
2022-11-30 13:02:55 -0500

    elephant  + falling apple
    tutorial up to 1.3

---
+++ hungry-animal-felixvrs-main/MyWorld.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The World our hero lives in.
+ * 
+ * @author Felix
+ * @version December 2022
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
+    }
+}
